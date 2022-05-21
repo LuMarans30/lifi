85953dbfb21f455a98ca35c51586d3e675d24857
LuMarans30 <68614754+LuMarans30@users.noreply.github.com>
2022-05-21 13:02:48 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+particlesJS.load('particles-js', 'assets/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+
+//Script che in base al verso di scorrimento della pagina mostra o nasconde la navbar
+//Se si sta scorrendo la pagina verso il basso allora la navbar viene nascosta
+//Altrimenti viene mostrata
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+    } else {
+        document.getElementById("nav").style.top = "-400px"; //La navbar rientra con una distanza di 400px dal top della pagina
+    }
+    prevScrollpos = currentScrollPos;
+}
