cbb9b5171ef980e3f2e780b1d8a9f300e242f924
Andrea Marano <andreamarano2004@gmail.com>
2022-08-03 12:26:13 +0200

    one tap sign in

--- js/app.js
+++ js/app.js
@@ -15,3 +15,35 @@ window.onscroll = function () {
     }
     prevScrollpos = currentScrollPos;
 }
+
+function handleCredentialResponse(response) {
+    // Here we can do whatever process with the response we want
+    // Note that response.credential is a JWT ID token
+    console.log("Encoded JWT ID token: " + response.credential);
+}
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "323824111948-scqqpbav49em320p2r7s21oojrbj04rt.apps.googleusercontent.com", // Replace with your Google Client ID
+        callback: handleCredentialResponse // We choose to handle the callback in client side, so we include a reference to a function that will handle the response
+    });
+    // You can skip the next instruction if you don't want to show the "Sign-in" button
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"), // Ensure the element exist and it is a div to display correcctly
+        { theme: "outline", size: "large" }  // Customization attributes
+    );
+    google.accounts.id.prompt(); // Display the One Tap dialog
+}
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "323824111948-scqqpbav49em320p2r7s21oojrbj04rt.apps.googleusercontent.com", // Replace with your Google Client ID
+        login_uri: "https://lumarans30.github.io/lifi" // We choose to handle the callback in server side, so we include a reference to a endpoint that will handle the response
+    });
+    // You can skip the next instruction if you don't want to show the "Sign-in" button
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"), // Ensure the element exist and it is a div to display correcctly
+        { theme: "outline", size: "large" }  // Customization attributes
+    );
+    google.accounts.id.prompt(); // Display the One Tap dialog
+}
