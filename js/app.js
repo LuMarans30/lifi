55c818f7938b0133e8420fb883c24c0c32d95f54
Andrea Marano <andreamarano2004@gmail.com>
2022-08-03 13:25:45 +0200

    one tap

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ particlesJS.load('particles-js', 'assets/particles.json', function () {
 //Se si sta scorrendo la pagina verso il basso allora la navbar viene nascosta
 //Altrimenti viene mostrata
 var prevScrollpos = window.pageYOffset;
+
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
@@ -14,36 +15,4 @@ window.onscroll = function () {
         document.getElementById("nav").style.top = "-400px"; //La navbar rientra con una distanza di 400px dal top della pagina
     }
     prevScrollpos = currentScrollPos;
-}
-
-function handleCredentialResponse(response) {
-    // Here we can do whatever process with the response we want
-    // Note that response.credential is a JWT ID token
-    console.log("Encoded JWT ID token: " + response.credential);
-}
-
-window.onload = function () {
-    google.accounts.id.initialize({
-        client_id: "323824111948-scqqpbav49em320p2r7s21oojrbj04rt.apps.googleusercontent.com", // Replace with your Google Client ID
-        callback: handleCredentialResponse // We choose to handle the callback in client side, so we include a reference to a function that will handle the response
-    });
-    // You can skip the next instruction if you don't want to show the "Sign-in" button
-    google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"), // Ensure the element exist and it is a div to display correcctly
-        { theme: "outline", size: "large" }  // Customization attributes
-    );
-    google.accounts.id.prompt(); // Display the One Tap dialog
-}
-
-window.onload = function () {
-    google.accounts.id.initialize({
-        client_id: "323824111948-scqqpbav49em320p2r7s21oojrbj04rt.apps.googleusercontent.com", // Replace with your Google Client ID
-        login_uri: "https://lumarans30.github.io/lifi" // We choose to handle the callback in server side, so we include a reference to a endpoint that will handle the response
-    });
-    // You can skip the next instruction if you don't want to show the "Sign-in" button
-    google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"), // Ensure the element exist and it is a div to display correcctly
-        { theme: "outline", size: "large" }  // Customization attributes
-    );
-    google.accounts.id.prompt(); // Display the One Tap dialog
 }
