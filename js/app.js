a92bf77ee286893a43f7122dcc69facffb77f8ce
Andrea Marano <andreamarano2004@gmail.com>
2022-08-04 15:50:56 +0200

    google sign in button

--- js/app.js
+++ js/app.js
@@ -17,41 +17,17 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
 
-var auth2;
-var googleUser; // The current user
-
-gapi.load('auth2', function () {
-    auth2 = gapi.auth2.init({
-        client_id: '323824111948-scqqpbav49em320p2r7s21oojrbj04rt.apps.googleusercontent.com'
-    });
-    auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
-
-    auth2.isSignedIn.listen(signinChanged);
-    auth2.currentUser.listen(userChanged); // This is what you use to listen for user changes
-});
-
-var signinChanged = function (val) {
-    console.log('Signin state changed to ', val);
-};
-
-var onSuccess = function (user) {
-    console.log('Signed in as ' + user.getBasicProfile().getName());
-    // Redirect somewhere
-};
-
-var onFailure = function (error) {
-    console.log(error);
-};
-
-function signOut() {
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
 }
-
-var userChanged = function (user) {
-    if (user.getId()) {
-        // Do something here
-    }
-};
